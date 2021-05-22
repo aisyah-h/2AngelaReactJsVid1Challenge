6f39c51c138d76734f12ce00413029ec509d263c
aisyah-h <83572065+aisyah-h@users.noreply.github.com>
2021-05-22 17:38:56 +0800

    Challenge done up! Lists of my favourite things!

--- src/index.js
+++ src/index.js
@@ -5,3 +5,34 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>A Few of My Favourite Things</h1>
+    {/*Note: this line shows how to comment in React JSX.*/}
+    {/*unordered list*/}
+    <h3>In no order:</h3>
+    <ul>
+      <li>cats</li>
+      <li>chocolates</li>
+      <li>mochi</li>
+      <li>milk tea</li>
+    </ul>
+    {/*ordered list*/}
+    <h3>In order:</h3>
+    <ol>
+      <li>cats</li>
+      <li>cats</li>
+      <li>cats</li>
+    </ol>
+    {/*description list*/}
+    <h3>If I had to describe them:</h3>
+    <dl>
+      <dt>1. Comel</dt>
+      <dd>- because she loves me back</dd>
+      <dt>2. Jamal</dt>
+      <dd>- because he lives with us and is a handsome burger</dd>
+    </dl>
+  </div>,
+  document.getElementById("root")
+);
